9312cf57cf87ec40de6521bc34b14a69b4881b1c
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2023-02-15 17:16:37 +0200

    finish

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import loader from './loader.svg';
+import logo from './logo.svg';
+import logoBg from './logo-no-background.svg';
+import logoCol from './logo-color.svg';
+import logoBlack from './logo-black.svg';
+export { logo, loader, logoBg, logoCol, logoBlack };
